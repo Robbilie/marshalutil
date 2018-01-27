149cedda9912585cd257a1034ba5bbf1c2a4d7e6
Robert Schuh <github@eneticum.de>
2018-01-27 18:38:21 +0100

    exclude tests for the iterator

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ module.exports = new Proxy({}, { get: (P,key) => {
     if (key === Symbol.iterator)
         return () => require("fs")
             .readdirSync(__dirname)
-            .filter(x => !x.startsWith("index") && x.split(".").slice(-1)[0].includes("js"))
+            .filter(x => !x.startsWith("index") && !x.includes(".test.") && x.split(".").slice(-1)[0].includes("js"))
             .map(x => require(`./${x}`))[Symbol.iterator]();
     else
         return require(`./${key}`);
